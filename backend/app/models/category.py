7927fcc8013ec66c5021de31f68a8ef3c024f01c
xxx051020 <3299792685@qq.com>
2026-06-25 13:35:53 +0800

    Add backend/app/models/category.py

---
+++ backend/app/models/category.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from ..database import Base
+
+class Category(Base):
+    __tablename__ = "categories"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50), unique=True, nullable=False)
+    
+    pois = relationship("POI", back_populates="category")
